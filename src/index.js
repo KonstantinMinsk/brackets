e22a1b863da2bf7074f1aa557a7a56fee1cfe3f0
KonstantinMinsk <Sokolov.k007@yandex.ru>
2020-03-14 18:00:24 +0300

    fix: i=-1, but not i--

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
 
   /*
-let brStr = bracketsConfig.flat(Infinity).join("").replace(/\s+/g, '');
-let strClear = str.replace(/\s+/g, '')
+let brStr = bracketsConfig.flat(Infinity).join("").replace(/\s+/g, ''); но тогда будет следующий массив "()[]"
+ПримерЖ 
+
+let strClear = str.replace(/\s+/g, '') - убираем пробелы
 
   // str.includes(arr[0].join('').replace(/\s+/g, '')) Смотрим включаеет ли строка в себя подмассив
   // подмассив делаем строкой join('') 
@@ -10,36 +12,36 @@ let strClear = str.replace(/\s+/g, '')
 
 
 // Как сравнить пары в строке и в массиве?
-
-let brNewStr = "";
-for (let i=0; i<bracketsConfig.length; i++) {
-  brNewStr += bracketsConfig[i].join("");
-}
-
-if (arr1 == true && arr2 == true && strClear.length === brStr.length) {
-  return true;
-} else return false
 */
 
+
+// Матрицу переделываем в массив так, чтобы подмассив стал arr[i]
 let newArr = [];
 bracketsConfig.forEach(element => {
-  // в новый массив добавляем element, который является подмассивом 
-  //по аналогии если бы let a = arrN[0].join("") и потом newArr.push(a)
-  newArr.push(element.join("")) 
+  newArr.push(element.join(""))      // можно использовать метод map let newArr = bracketsConfig.map( (b) => b.join("") 
 });
+// в новый массив добавляем element, который является подмассивом 
+  //по аналогии если бы let a = arrN[0].join("") и потом newArr.push(a)
+  // Exempale: было[[ ' (' , ' ) ' ], [ ' [ ' , ' ] ' ]]   стало -    [" (  ) ", " [  ] "]
+
+
 
 // далее проверяем содержит ли строка элемент массива, который у нас у же тоже строка [" (  ) ", " [  ] "]
-
+// let strClear = str.replace(/\s+/g, '') если убрать пробелы тоже самое
 for (let i = 0; i<newArr.length; i++) {
-  if( str.includes(newArr[i].replace(/\s+/g, '')) ) {
-     str.replace(newArr[i], ""); // Возращаем строку без пары newArr[i] - (  )
-     i--; // возвращаемся еще раз проверить
+  if( str.includes(newArr[i]) ) {
+    str = str.replace(newArr[i], "");  
+    i =-1; 
+    // Возращаем строку без пары newArr[i] - (  ) 
+    // Почему наличеи пробелов не влияет? (newArr[i].replace(/\s+/g, ''), "") - не дает никакого результата
+    // i--; возвращаемся еще раз проверить
   }
 }
 
 if (str.length == 0) {
   return true;
-} else return false;
+} else return false; 
 
+// короткая запись return (str) ? false : true;
 
 }
