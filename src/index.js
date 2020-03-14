50c40d269d8a7071c2290468761d69683423882a
KonstantinMinsk <Sokolov.k007@yandex.ru>
2020-03-14 17:26:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  /*
+let brStr = bracketsConfig.flat(Infinity).join("").replace(/\s+/g, '');
+let strClear = str.replace(/\s+/g, '')
+
+  // str.includes(arr[0].join('').replace(/\s+/g, '')) Смотрим включаеет ли строка в себя подмассив
+  // подмассив делаем строкой join('') 
+  // и из строки удаляем пробелы replace(/\s+/g, '') - метод возвращает новую строку без пробелов 
+
+
+// Как сравнить пары в строке и в массиве?
+
+let brNewStr = "";
+for (let i=0; i<bracketsConfig.length; i++) {
+  brNewStr += bracketsConfig[i].join("");
+}
+
+if (arr1 == true && arr2 == true && strClear.length === brStr.length) {
+  return true;
+} else return false
+*/
+
+let newArr = [];
+bracketsConfig.forEach(element => {
+  // в новый массив добавляем element, который является подмассивом 
+  //по аналогии если бы let a = arrN[0].join("") и потом newArr.push(a)
+  newArr.push(element.join("")) 
+});
+
+// далее проверяем содержит ли строка элемент массива, который у нас у же тоже строка [" (  ) ", " [  ] "]
+
+for (let i = 0; i<newArr.length; i++) {
+  if( str.includes(newArr[i].replace(/\s+/g, '')) ) {
+     str.replace(newArr[i], ""); // Возращаем строку без пары newArr[i] - (  )
+     i--; // возвращаемся еще раз проверить
+  }
+}
+
+if (str.length == 0) {
+  return true;
+} else return false;
+
+
 }
